013daecb546da1a0109a0aac71db4e7eac3587ab
Nathan Hyde <nhyde@mbayaq.org>
2020-05-20 16:07:10 -0700

    Make lifeguardOut work with local-sync.xml files.
    Added a few conditional statements to switch between what is required for local vs current sync files.
    Also added a --dry-run flag to the options to just print what would happen when run.

--- brightsignlifeguard/lifeguardOut/lifeguardOut.py
+++ brightsignlifeguard/lifeguardOut/lifeguardOut.py
@@ -10,10 +10,17 @@ from copyfile import copyFile
 def theprogram():
     parser = argparse.ArgumentParser()
     parser.add_argument("presentation_directory",
+        # nargs=1,
         help="Root directory containing Brightsign presentation (current-sync.xml + pool/).")
+    parser.add_argument('--dry-run',
+        action='store_true',
+        required=False,
+        help='Just print what would happen. No modifications made.',
+        default=False,
+        dest='dry_run')
     args = parser.parse_args()
     PRESENTATION_LOCATION = args.presentation_directory
-
+    DRY_RUN = args.dry_run
 
     if os.path.isdir(PRESENTATION_LOCATION):
         if PRESENTATION_LOCATION[-1] != '/':
@@ -26,19 +33,34 @@ def theprogram():
         os.mkdir(PRESENTATION_LOCATION+"kiddie_pool")
 
 
-    if not os.path.isfile(PRESENTATION_LOCATION + 'current-sync.xml'):
-        print("""Presentation directory is missing a current-sync.xml file.
+    sync_file = 'current-sync.xml'
+    if not os.path.isfile(PRESENTATION_LOCATION + sync_file):
+        sync_file = 'local-sync.xml'
+
+    if not os.path.isfile(PRESENTATION_LOCATION + sync_file):
+        print("""Presentation directory is missing a current-sync.xml or local-sync.xml file.
             Probably not a full Brightsign Presentation folder""")
+        exit()
 
-
-    tree = ElementTree.parse(PRESENTATION_LOCATION + 'current-sync.xml')
+    tree = ElementTree.parse(PRESENTATION_LOCATION + sync_file)
     root = tree.getroot()
-    baseurl = root.find('./meta/client/base').text
+    if sync_file == 'current-sync.xml':
+        baseurl = root.find('./meta/client/base').text
+    else:
+        baseurl = PRESENTATION_LOCATION
+
     for elem in root.findall('.//download'):
         name = elem.find('.//name').text
         urlpath = elem.find('.//link').text
-        filepath = PRESENTATION_LOCATION + urlpath[len(baseurl)+1:]
+        if sync_file == 'current-sync.xml':
+            filepath = PRESENTATION_LOCATION + urlpath[len(baseurl)+1:]
+        else:
+            filepath = PRESENTATION_LOCATION + urlpath
 
-        copyFile(os.path.abspath(filepath), os.path.abspath(PRESENTATION_LOCATION+"kiddie_pool/"+name))
+        outpath = os.path.abspath(PRESENTATION_LOCATION+"kiddie_pool/"+name)
+        if DRY_RUN:
+            print('Would copy ' + filepath + ' to ' + outpath)
+        else:
+            copyFile(os.path.abspath(filepath), outpath)
 
     print("Adult swim is over. Presentation files are now in the kiddle_pool.")
